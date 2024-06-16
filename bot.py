5409d411544f58ce93d67b16371396e3880e0846
Edmund Lin <elz.work22@gmail.com>
2024-06-16 16:51:34 +0800

    feat: use HTML parse mode as a test

--- bot.py
+++ bot.py
@@ -54,14 +54,13 @@ async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     
     await context.bot.send_photo(
         chat_id=user_id,
-        photo=image_url,
-        parse_mode=constants.ParseMode.MARKDOWN_V2
+        photo=image_url
     )
 
     await context.bot.send_message(
         chat_id=user_id,
-        text=f"Here is your generated image\\! Click [here]({share_link}) to share the image on Instagram\\.",
-        parse_mode=constants.ParseMode.MARKDOWN_V2
+        text=f"Here is your generated image! <a href='{share_link}'>Click here</a> to share the image on Instagram.",
+        parse_mode=constants.ParseMode.HTML
     )
 
 def main():
