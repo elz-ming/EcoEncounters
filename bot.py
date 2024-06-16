c3c52ebc44185456aee898402645b3e94d801e89
Edmund Lin <elz.work22@gmail.com>
2024-06-16 16:41:54 +0800

    fix: update the generate image function

--- bot.py
+++ bot.py
@@ -43,33 +43,23 @@ async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     await query.answer()
     user_id = query.message.chat.id
 
-    # Generate the image
-    image_url = 'https://mighty-macaque-30f16fdc84f3.herokuapp.com/images/badge_1'
+    image_name = "badge_1"
+
+    # Extract the image URL from the callback data
+    image_url = f'https://mighty-macaque-30f16fdc84f3.herokuapp.com/images/{image_name}'
+
+    # Escape the URL for Markdown V2
+    escaped_image_url = image_url.replace('.', '\\.').replace('-', '\\-')
+    share_link = f'instagram://story-camera?AssetPath={escaped_image_url}'
     
     await context.bot.send_photo(
         chat_id=user_id,
         photo=image_url,
-        caption="Here is your generated image!",
+        caption=f"Here is your generated image! Click [here]({share_link}) to share the image on Instagram.",
+        parse_mode=constants.ParseMode.MARKDOWN_V2,
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("Share to Instagram", callback_data=f'share_to_instagram')]
         ])
-    )
-
-async def share_to_instagram(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    await query.answer()
-    user_id = query.message.chat.id
-
-    # Extract the image URL from the callback data
-    image_url = 'https://mighty-macaque-30f16fdc84f3.herokuapp.com/images/badge_1'
-
-    # Provide the link to share the image to Instagram
-    share_link = f'instagram://story-camera?AssetPath={image_url}'
-    
-    await context.bot.send_message(
-        chat_id=user_id,
-        text=f"Click [here]({share_link}) to share the image on Instagram.",
-        parse_mode=constants.ParseMode.MARKDOWN_V2
     )
 
 def main():
@@ -80,7 +70,6 @@ def main():
     # Add handlers for the /start command and callbacks
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(generate_image, pattern='generate_image'))
-    application.add_handler(CallbackQueryHandler(share_to_instagram, pattern='share_to_instagram'))
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
     application.run_polling()
